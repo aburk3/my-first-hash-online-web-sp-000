b2d04267e71440cf22b1eb20bb88f0dac8f66efa
Austin  Burke <aburk3@gmail.com>
2018-10-02 11:03:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash = {"my_name" => "Austin", "my_age" => 24, "my_weight" => 172}
+  return my_hash
 end
 
 
